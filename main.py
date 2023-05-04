eff5d0e045a28f313891ca7ebefcadd18e5d7625
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2023-05-04 13:43:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,87 @@
+import cv2
+import pandas as pd
+import numpy as np
+from ultralytics import YOLO
+from tracker import*
+import cvzone
+
+model=YOLO('yolov8s.pt')
+
+
+
+def RGB(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE :  
+        colorsBGR = [x, y]
+        print(colorsBGR)
+        
+
+cv2.namedWindow('RGB')
+cv2.setMouseCallback('RGB', RGB)
+
+cap=cv2.VideoCapture('shoppingmall.mp4')
+
+
+my_file = open("coco.txt", "r")
+data = my_file.read()
+class_list = data.split("\n") 
+#print(class_list)
+
+k=['car','motorcycle']
+count=0
+
+tracker=Tracker()
+
+area1=[(708,239),(690,253),(945,334),(959,317)]
+area2=[(681,257),(677,265),(927,353),(937,342)]
+
+people_enter={}
+counter1=[]
+
+people_exit={}
+counter2=[]
+while True:    
+    ret,frame = cap.read()
+    if not ret:
+        break
+    count += 1
+    if count % 3 != 0:
+        continue
+    frame=cv2.resize(frame,(1020,500))
+   
+
+    results=model.predict(frame)
+ #   print(results)
+    a=results[0].boxes.boxes
+    px=pd.DataFrame(a).astype("float")
+#    print(px)
+    list=[]         
+    for index,row in px.iterrows():
+#        print(row)
+ 
+        x1=int(row[0])
+        y1=int(row[1])
+        x2=int(row[2])
+        y2=int(row[3])
+        d=int(row[5])
+        c=class_list[d]
+        if 'person' in c:
+            list.append([x1,y1,x2,y2])
+    bbox_id=tracker.update(list)
+    for bbox in bbox_id:
+        x3,y3,x4,y4,id=bbox
+        cv2.rectangle(frame,(x3,y3),(x4,y4),(255,0,255),1)
+        cv2.circle(frame,(x4,y4),4,(255,0,0),-1)
+        cvzone.putTextRect(frame,f'{id}',(x3,y3),1,2)
+        
+    cv2.polylines(frame,[np.array(area1,np.int32,)],True,(0,0,255),1)
+    cv2.polylines(frame,[np.array(area2,np.int32,)],True,(0,255,0),1)    
+   
+   
+
+ 
+    cv2.imshow("RGB", frame)
+    if cv2.waitKey(1)&0xFF==27:
+        break
+cap.release()
+cv2.destroyAllWindows()
+
